4a668d3aa9a49b2f5a51ca19e26d83920cfdafac
Jeongseok Lee (JS) <jslee02@gmail.com>
2013-12-27 01:02:37 -0500

    Update before_install.sh

--- ci/before_install.sh
+++ ci/before_install.sh
@@ -1,23 +1,20 @@
-before_install() {
-  cd /tmp
-  PROJECTS=''
-  for REPO in $PROJECTS
-  do
-    git clone git://github.com/dartsim/$REPO.git
-    (cd $REPO; cmake .; make && sudo make install)
-  done
+# Set github and bitbucket free of host checking questions
+echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+echo -e "Host bitbucket.org\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 
-  # Set github and bitbucket free of host checking questions
-  echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
-  echo -e "Host bitbucket.org\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+# Install DART
+git clone https://github.com/dartsim/dart.git
+(cd dart; ci/before_install.sh; cmake .; make && sudo make install)
 
-  # Install DART
-  git clone https://github.com/dartsim/dart.git
-  (cd dart; ci/before_install.sh; cmake .; make && sudo make install)
-}
-
-APT='freeglut3 freeglut3-dev libglu1-mesa-dev libxmu-dev libxi-dev libwxgtk2.8-dev
- libboost-all-dev cmake cmake-curses-gui libgtest-dev libqt4-dev libopenscenegraph-dev'
+APT='
+freeglut3-dev
+libboost-all-dev
+libglu1-mesa-dev
+libgtest-dev
+libopenscenegraph-dev
+libqt4-dev
+libxmu-dev
+libxi-dev
+'
 
 sudo apt-get install $APT
-(before_install)
