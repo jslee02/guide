5eb7279a17dad0a5d995fafa06db7747cc8f5df8
Jeongseok Lee <jslee02@gmail.com>
2016-05-07 17:12:01 -0400

    Travis: install libdart5-dev

--- ci/before_install.sh
+++ ci/before_install.sh
@@ -1,13 +1,8 @@
-# Set github and bitbucket free of host checking questions
-echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
-echo -e "Host bitbucket.org\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
-
 # Install DART
 sudo apt-add-repository --yes ppa:libccd-debs
 sudo apt-add-repository --yes ppa:fcl-debs
 sudo apt-add-repository --yes ppa:dartsim
 sudo apt-get update
-sudo apt-get install libdart4-dev
 
 APT='
 freeglut3-dev
@@ -18,8 +13,7 @@ libopenscenegraph-dev
 libqt4-dev
 libxmu-dev
 libxi-dev
-libois-dev
-libogre-1.7.4-dev
+libdart5-dev
 '
 
-sudo apt-get install $APT
+sudo apt-get -qq --yes --force-yes install $APT
